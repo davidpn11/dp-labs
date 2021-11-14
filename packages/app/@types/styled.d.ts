0994b571f6f3dc7a913444c0e691da3a74e02051
David Nascimento <davidpn.11@gmail.com>
2021-11-14 14:25:43 -0700

    try fix types errors

--- packages/app/@types/styled.d.ts
+++ packages/app/@types/styled.d.ts
@@ -1,6 +1,8 @@
 import 'styled-components';
 import type {ThemeWithMode} from '@dplabs/shared';
+import type {Theme} from '@dplabs/qik-ui';
 
+type GeneralTheme = ThemeWithMode | Theme;
 declare module 'styled-components' {
-  interface DefaultTheme extends ThemeWithMode {}
+  interface DefaultTheme extends GeneralTheme {}
 }
